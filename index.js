b311c287b7cca48fc44e3b4860ffdf8a04c2719e
syairaarif <putrisyafira4444@gmail.com>
2025-11-04 13:53:40 +0700

    membuat codingan supaya nanti server bisa dijalankan

--- index.js
+++ index.js
@@ -2,3 +2,19 @@ const express = require('express');
 const app = express();
 const db = require('./models');
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(PORT, async () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+db.sequelize.sync().then((result) => {
+    app.listen(3000, () => {
+        console.log('Server Started');
+    })
+})
+    .catch((err) => {
+        console.log(err);
+    })
