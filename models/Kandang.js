b30b3f58714556860ca561d81043cf0f678fa8b3
syairaarif <putrisyafira4444@gmail.com>
2025-11-04 13:51:08 +0700

    menambahkan codingan di bagian folder models dan buat file bernama kandang.js isinya untuk mengenalkan colom yang ada di table kandang

---
+++ models/Kandang.js
@@ -0,0 +1,39 @@
+module.exports = (sequelize, DataTypes) => {
+    const Kandang = sequelize.define('Kandang', {
+        ID: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+
+        Nama_Hewan: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+
+        nama_petugas: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+
+        Usia_hewan: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+
+        Jenis_hewan: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+
+        Tahun_lahir: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        }
+    }, {
+        tableName: 'kandang',
+        timestamps: false,
+        freezeTableName: true
+    });
+    return Kandang;
+}
